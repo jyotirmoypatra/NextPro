da325d1c5a050ce63f8a0a31c49f9ff09c580dd2
JYOTIRMOY PATRA <jyotirmoypatra@JYOTIRMOYs-MacBook-Air.local>
2025-12-04 20:13:07 +0530

    devices page add

--- NextPro/Admin/Home/DeviceAdminTabView.swift
+++ NextPro/Admin/Home/DeviceAdminTabView.swift
@@ -7,37 +7,102 @@
 
 import SwiftUI
 
+struct DeviceModel: Identifiable, Hashable {
+    let id = UUID()
+    let title: String
+    let doorName: String
+    let status: String
+}
+
+let sampledevices: [DeviceModel] = [
+  
+    DeviceModel(title: "TC434(266253636)",doorName: "UTL-3S/DOOR1", status: "ONLINE"),
+    DeviceModel(title: "TC434(266253636)",doorName: "UTL-3S/DOOR1", status: "ONLINE"),
+    DeviceModel(title: "TC434(266253636)",doorName: "UTL-3S/DOOR1", status: "OFFLINE"),
+    DeviceModel(title: "TC434(266253636)",doorName: "UTL-3S/DOOR1", status: "ONLINE"),
+]
+
+
 struct DeviceAdminTabView: View {
+    let devices =  sampledevices
+
     var body: some View {
-        VStack(spacing: 16) {
-            Text("Device List!")
-                .font(.headline)
-                .foregroundColor(.white)
-
-            Text("Looks like you do not have a Device yet")
-                .font(.subheadline)
-                .foregroundColor(.gray)
-
-            Button(action: {
-                // Add Facility Action
-            }) {
-                HStack {
-                    Image(systemName: "plus")
-                    Text("Add Device")
+        ZStack {
+            VStack{
+                Button(action: {
+                   print("add devices")
+                }) {
+                    HStack{
+                        Image(systemName: "plus")
+                            .font(.system(size: 18))
+                            .foregroundColor(.white)
+                            .fontWeight(.bold)
+                          
+                        Text("Add Devices")
+                            .font(.custom("Inter-SemiBold", size: 16))
+                            .foregroundColor(.white)
+                    }
+                   
                 }
-                .font(.headline)
                 .padding()
-                .frame(maxWidth: .infinity)
-                .background(Color.white)
-                .foregroundColor(.black)
-                .cornerRadius(10)
+                
+                ScrollView {
+                    VStack(alignment: .leading, spacing: 20) {
+                        ForEach(devices) { item in
+                            DeviceCardView(device: item)
+                                .onTapGesture {
+                                    // onSelect(item)
+                                }
+                        }
+                        Spacer()
+                    }
+                    .padding(.horizontal, 20)
+                    .padding(.top, 10)
+                }
             }
-            .padding(.horizontal, 30)
+            .padding(.top,20)
         }
-        .padding(.vertical, 30)
-        .padding(.horizontal)
-        .background(Color.black.opacity(0.4))
-        .cornerRadius(16)
-        .padding(.horizontal, 20)
+        .navigationBarHidden(true)
+    }
+}
+
+
+struct DeviceCardView: View {
+    let device: DeviceModel
+
+    var body: some View {
+        HStack(spacing: 16) {
+            Image("smartphone")
+                .resizable()
+                .frame(width: 33, height: 33)
+
+            VStack(alignment: .leading, spacing: 2) {
+                Text(device.title)
+                    .font(.custom("Inter-SemiBold", size: 16))
+                    .foregroundColor(.white)
+                    .lineLimit(2)
+
+                Text(device.doorName)
+                    .font(.custom("Inter-Regular", size: 14))
+                    .foregroundColor(.gray.opacity(0.9))
+                    .lineLimit(2)
+            }
+
+            Spacer()
+
+            Text(device.status)
+                .font(.custom("Inter-SemiBold", size: 10))
+                .foregroundColor(.white)
+                .padding(6)
+                .background(device.status == "ONLINE" ? Color.green : Color.red)
+                .cornerRadius(6)
+        }
+        .padding(20)
+        .background(Color.white.opacity(0.08))
+        .cornerRadius(14)
+        .overlay(
+            RoundedRectangle(cornerRadius: 14)
+                .stroke(Color.white.opacity(0.15), lineWidth: 1)
+        )
     }
 }
