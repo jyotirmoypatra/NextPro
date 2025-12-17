cbf14a7c96ce4d3a70009a1966738093a287da64
JYOTIRMOY PATRA <jyotirmoypatra@JYOTIRMOYs-MacBook-Air.local>
2025-12-17 10:19:11 +0530

    webview old code remove

--- NextPro/Utils/WebViewModal.swift
+++ NextPro/Utils/WebViewModal.swift
@@ -73,64 +73,7 @@ struct WebViewModal: View {
     }
 }
 
-// MARK: - WebView Wrapper
-//struct WebView: UIViewRepresentable {
-//    let url: String
-//    @Binding var isLoading: Bool
-//    @Environment(\.colorScheme) var colorScheme
-//    
-//    func makeUIView(context: Context) -> WKWebView {
-//        let bg = colorScheme == .dark ? UIColor.black : UIColor.white
-//        let webView = WKWebView()
-//        webView.navigationDelegate = context.coordinator
-//        webView.backgroundColor = bg
-//        webView.scrollView.backgroundColor = bg
-//        
-//        return webView
-//    }
-//    
-//    func updateUIView(_ webView: WKWebView, context: Context) {
-//        // Only load if not already loaded
-//        if webView.url == nil {
-//            if let url = URL(string: url) {
-//                let request = URLRequest(url: url)
-//                webView.load(request)
-//            }
-//        }
-//    }
-//    
-//    func makeCoordinator() -> Coordinator {
-//        Coordinator(self)
-//    }
-//    
-//    class Coordinator: NSObject, WKNavigationDelegate {
-//        var parent: WebView
-//        
-//        init(_ parent: WebView) {
-//            self.parent = parent
-//        }
-//        
-//        func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
-//            print("🌐 WebView started loading")
-//            parent.isLoading = true
-//        }
-//        
-//        func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-//            print("✅ WebView finished loading")
-//            parent.isLoading = false
-//        }
-//        
-//        func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
-//            parent.isLoading = false
-//            print("WebView failed to load: \(error.localizedDescription)")
-//        }
-//        
-//        func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
-//            parent.isLoading = false
-//            print("WebView failed provisional navigation: \(error.localizedDescription)")
-//        }
-//    }
-//}
+
 
 
 
