4469e241071f84730f082a233f433f40f6c58a30
JYOTIRMOY PATRA <jyotirmoypatra@JYOTIRMOYs-MacBook-Air.local>
2025-12-30 13:16:18 +0530

    no internet ui fix

--- NextPro/EndUser/View/HomeViewEndUser.swift
+++ NextPro/EndUser/View/HomeViewEndUser.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct HomeViewEndUser: View {
     @State private var selectedTab = 0
+    private let tabBarHeight: CGFloat = 55
 
     var body: some View {
         GeometryReader { geo in
@@ -32,61 +33,70 @@ struct HomeViewEndUser: View {
                             DoorOpenView()
                                 .navigationBarBackButtonHidden(true)
                                 .navigationBarHidden(true)
+                                .frame(
+                                                maxWidth: .infinity,
+                                                maxHeight: .infinity
+                                            )
                         case 1:
                             MembershipEndUserView()
                                 .navigationBarBackButtonHidden(true)
                                 .navigationBarHidden(true)
+                                .frame(height: geo.size.height - tabBarHeight)
+
                         case 2:
                             ProfileEndUserView()
                                 .navigationBarBackButtonHidden(true)
                                 .navigationBarHidden(true)
+                                .frame(height: geo.size.height - tabBarHeight)
+
                         default:
                             EmptyView()
                         }
                     }
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    //.frame(maxWidth: .infinity, maxHeight: .infinity)
                     .transition(.opacity)
                     .animation(.easeInOut(duration: 0.25), value: selectedTab)
 
-                    Divider()
-                        .background(Color.white.opacity(0.15))
-
                     
-                    //tab
-                    HStack {
-                        TabBarItemUser(
-                            title: "Open Doors",
-                            activeIcon: "key-active",
-                            inactiveIcon: "key-inactive",
-                            isSelected: selectedTab == 0
-                        )
-                        .onTapGesture { selectedTab = 0 }
-
-                        Spacer()
-
-                        TabBarItemUser(
-                            title: "Membership",
-                            activeIcon: "user-star-active",
-                            inactiveIcon: "user-star-inactive",
-                            isSelected: selectedTab == 1
-                        )
-                        .onTapGesture { selectedTab = 1 }
-
-                        Spacer()
-
-                        TabBarItemUser(
-                            title: "Profile",
-                            activeIcon: "profile-circle-active",
-                            inactiveIcon: "profile-circle-inactive",
-                            isSelected: selectedTab == 2
-                        )
-                        .onTapGesture { selectedTab = 2 }
+                    //tabv
+                    VStack(spacing: 10){
+                        Divider()
+                            .background(Color.white.opacity(0.15))
+                        HStack {
+                            TabBarItemUser(
+                                title: "Open Doors",
+                                activeIcon: "key-active",
+                                inactiveIcon: "key-inactive",
+                                isSelected: selectedTab == 0
+                            )
+                            .onTapGesture { selectedTab = 0 }
+                            
+                            Spacer()
+                            
+                            TabBarItemUser(
+                                title: "Membership",
+                                activeIcon: "user-star-active",
+                                inactiveIcon: "user-star-inactive",
+                                isSelected: selectedTab == 1
+                            )
+                            .onTapGesture { selectedTab = 1 }
+                            
+                            Spacer()
+                            
+                            TabBarItemUser(
+                                title: "Profile",
+                                activeIcon: "profile-circle-active",
+                                inactiveIcon: "profile-circle-inactive",
+                                isSelected: selectedTab == 2
+                            )
+                            .onTapGesture { selectedTab = 2 }
+                        }
+                        .padding(.horizontal, 30)
                     }
-
-                    .padding(.top,10)
-                    .padding(.horizontal, 30)
+                    .frame(height: tabBarHeight)
                     .background(Color.black.opacity(0.9))
                     .ignoresSafeArea(edges: .bottom)
+                   
                 }
                 .frame(width: geo.size.width, height: geo.size.height)
             }
